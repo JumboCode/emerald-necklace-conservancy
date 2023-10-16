18a5903b9026ec5f64b4106b3da70180d8a1609f
Benjamin-Skinner <benjamin.kean.skinner@gmail.com>
2023-10-15 20:25:05 -0400

    Added boilerplate code

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+/*
+    Import the button from 'components/Button.tsx'
+    Render the button below the h1 tag, passing it the appropriate props
+*/
+export default function Home() {
+	return (
+		<div>
+			{
+				<h1>Emerald Necklace Button</h1>
+				/* Render the button here */
+			}
+		</div>
+	)
+}
