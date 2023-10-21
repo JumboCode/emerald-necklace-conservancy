993ef1364e0defdb4216d8709882b0eaa8cfcb96
Snw125 <123609058+Snw125@users.noreply.github.com>
2023-10-20 20:46:50 -0400

    Button DONE

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,19 @@
     Import the button from 'components/Button.tsx'
     Render the button below the h1 tag, passing it the appropriate props
 */
+"use client";
+import MyButton from "/src/components/Button.tsx";
+
+
 export default function Home() {
-	return (
-		<div>
+	
+
+    return (
+		<div class="p-6 max-w-xs mx-auto bg-white rounded-xl shadow-lg flex justify-center">
 			{
-				<h1>Emerald Necklace Button</h1>
-				/* Render the button here */
+                <h1>
+                    <MyButton label="Emerald Necklace Button!" />
+                </h1>
 			}
 		</div>
 	)
