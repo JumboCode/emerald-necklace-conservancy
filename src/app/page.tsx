8af22b49e7d4c2aa9bfa6ed20ac4507c7848179a
Vedant Modi <vedantmodi@gmail.com>
2023-10-22 20:58:47 -0400

    Add final button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
 /*
-    Import the button from 'components/Button.tsx'
-    Render the button below the h1 tag, passing it the appropriate props
+	Import the button from 'components/Button.tsx'
+	Render the button below the h1 tag, passing it the appropriate props
 */
+
+"use client";
+import WeatherButton from "@/components/Button";
+
 export default function Home() {
 	return (
 		<div>
 			{
-				<h1>Emerald Necklace Button</h1>
-				/* Render the button here */
+				// <h1>Emerald Necklace Button</h1>
+				<h1>
+					<WeatherButton className="button" label="Weather is awesome!" />
+				</h1>
+
+				
+				
 			}
 		</div>
-	)
+	);
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,29 @@
+import { Hanken_Grotesk } from "next/font/google";
+import { useState } from "react"
+
+export default function WeatherButton(props) {
+    const [unit, setUnit] = useState([])
+    const [temperature, setTemp] = useState([])
+    const [shortForecast, setForecast] = useState([])
+
+
+	const handleClick = () => {
+		fetch("https://api.weather.gov/gridpoints/BOX/69,92/forecast")
+			.then((res) => res.json())
+			.then((json) => {
+				console.log(json);
+				setTemp(json["properties"]["periods"][0]["temperature"])
+				setForecast(json["properties"]["periods"][0]["shortForecast"])
+                setUnit(json["properties"]["periods"][0]["temperatureUnit"])
+			});
+	}
+
+    return (
+        <div>
+            <p onClick={handleClick} className="button">{props.label}</p>
+            <p className="text-center">{temperature} {unit}</p>
+            <p className="text-center">{shortForecast}</p>
+        </div>
+    )
+
+}
