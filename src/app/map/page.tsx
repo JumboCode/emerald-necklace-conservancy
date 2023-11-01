75530aa712f9d691ce0a3b225a5375d71fc54e7b
Vedant Modi <vedantmodi@gmail.com>
2023-11-01 15:41:40 -0400

    added map page to app

---
+++ src/app/map/page.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import React from "react";
+
+export default function MapPage() {
+
+    return (
+        <div>
+
+            <h2>Map</h2>
+            <br/>
+            <ul>
+                <li>
+                    <a href="/franklin-park"> Franklin Park </a>
+                </li>
+                <li>
+                    <a href="/arnold-arboretum"> Arnold Arboretum </a>
+                </li>
+                <li>
+                    <a href="/jamaica-pond"> Jamaica Pond </a>
+                </li>
+                <li>
+                    <a href="/olmsted-park"> Olmstead Park </a>
+                </li>
+                <li>
+                    <a href="/riverway"> Riverway </a>
+                </li>
+                <li>
+                    <a href="/back-bay-fens"> Back Bay Fens </a>
+                </li>
+                <li>
+                    <a href="/public-garden-and-commons"> Public Garden and Commons </a>
+                </li>
+            </ul>
+        </div>
+    );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
     Import the button from 'components/Button.tsx'
     Render the button below the h1 tag, passing it the appropriate props
 */
+
 export default function Home() {
 	return (
 		<div>
