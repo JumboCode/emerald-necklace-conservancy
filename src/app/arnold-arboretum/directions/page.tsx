9e2cf71c179f0bf33d08f2eadc20e9d7385ae5c9
Snw125 <123609058+Snw125@users.noreply.github.com>
2023-11-02 17:54:32 -0400

    Attractions and Directions
    wowee look at those files and folders

---
+++ src/app/arnold-arboretum/directions/page.tsx
@@ -0,0 +1,16 @@
+/*
+    arnold-arboretum/directions
+*/
+'use client'
+
+import React from "react";
+
+export default function ArnArbDirections() {
+	return (
+		<div>
+			{
+				<h1>Arnold Arboretum Directions</h1>
+			}
+		</div>
+	)
+}
---
+++ src/app/olmsted-park/directions/page.tsx
@@ -0,0 +1,16 @@
+/*
+    olmsted-park/directions
+*/
+'use client'
+
+import React from "react";
+
+export default function OlmstedDirections() {
+	return (
+		<div>
+			{
+				<h1>Olmsted Park Directions</h1>
+			}
+		</div>
+	)
+}
