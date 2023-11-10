84e913085db0a1a8be04883abfd1ebde735802a9
Nicholas Chen <nicholas.k.y.chen@gmail.com>
2023-11-10 14:11:52 -0500

    Finished map

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,12 +9,10 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+          'map': "url('/img/map.png')",
+        },
       },
     },
-  },
   plugins: [],
 }
 export default config
