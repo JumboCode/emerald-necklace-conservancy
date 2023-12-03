ffc39c285dc7655d56f87301bac3be5befbb84d2
dberge0 <danielebergen@gmail.com>
2023-12-03 18:48:57 -0500

    Added splash screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
+'use client'
 import ParkName from '@/components/ParkName'
 import MapPin from '@/components/MapPin'
 import ParkNav from '../components/ParkNav'
 import React from 'react'
 import MapPage from './map/page';
-import Background from '@/components/Background';
+import WelcomePage from '../components/WelcomePage'
 
 function App() {
 	const ParkID1 = 'franklin-park'
 	return (
 		<>
-			<Background />
-			{/* <MapPage /> */}
+			<WelcomePage />
 		</>
 
 	)
