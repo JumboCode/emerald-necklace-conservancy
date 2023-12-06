b6ff3151fedbe2cc5d26d05c623afbd2df9e3ede
Vedant Modi <vedantmodi@gmail.com>
2023-12-06 16:00:58 -0500

    Add new ParkTitle module, and include it in park history pages

---
+++ src/app/arnold-arboretum/page.tsx
@@ -0,0 +1,23 @@
+/*
+    arnold-arboretum/
+*/
+'use client'
+import React from "react"
+import ParkTitle from "@/components/ParkTitle"
+
+export default function ArnArbHistory() {
+    return (
+        <div>
+            <ParkTitle
+                title="Arnold Arboretum"
+                bodyText="Established in 1872, the Arnold Arboretum is open 
+                          daily to the public as a free landscape for the study 
+                          and enjoyment of trees, shrubs, and vines. 
+                          As North America’s first public arboretum and a 
+                          National Historic Landmark, it is owned by the City 
+                          of Boston and managed by Harvard University under a 
+                          1,000-year lease signed in 1882.."
+            />
+        </div>
+    )
+}
--- src/app/franklin-park/page.tsx
+++ src/app/franklin-park/page.tsx
@@ -1,1 +1,17 @@
-x
+/*
+    franklin-park/
+*/
+'use client'
+import React from "react"
+import ParkTitle from "@/components/ParkTitle"
+
+export default function FraParkHistory() {
+    return (
+        <div>
+            <ParkTitle
+                title="Franklin Park"
+                bodyText="Treasures abound throughout Olmsted’s “country park” and at 527 acres, it is the largest park in the Emerald Necklace. Named for Benjamin Franklin, the park brings together rural scenery, spectacular rock outcroppings, a woodland preserve, expansive pastoral vistas and an area for active recreation and sports."
+            />
+        </div>
+    )
+}
