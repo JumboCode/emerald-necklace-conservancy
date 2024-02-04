770798f110b682af0a1c66522385ccaabe99828b
Vedant Modi <vedantmodi@gmail.com>
2024-02-04 12:47:23 -0500

    Add new attraction components in @/components/

--- src/components/ParkTitle.tsx
+++ src/components/ParkTitle.tsx
@@ -3,15 +3,13 @@ import React from 'react'
   
 interface ParkTitleProps {
     title: string;
-    bodyText: string;
-  }
+}
   
-  const ParkTitle: React.FC<ParkTitleProps> = ({ title, bodyText }) => {
+  const ParkTitle: React.FC<ParkTitleProps> = ({ title }) => {
     return (
       <div className="absolute left-10 mt-10 ml-10">
         <div className = "p-6 max-w-sm bg-custom-white rounded-lg">
           <h1 className="text-2xl font-semibold font-Inter mb-4">{title}</h1>
-            <p>{bodyText}</p>
         </div>
       </div>
     );
