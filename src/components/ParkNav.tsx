f918889c7dea6763889d9639390479953eeb17d9
dberge0 <danielebergen@gmail.com>
2024-03-03 19:29:18 -0500

    increased size of nav bar

--- src/components/ParkNav.tsx
+++ src/components/ParkNav.tsx
@@ -10,7 +10,7 @@ const NavButton: React.FC<NavButtonProps> = ({ label, selected, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className={`px-4 py-2 text-sm font-semibold p-0 ${
+      className={`px-6 py-4 text-sm text-xl p-0 ${
         selected ? 'bg-custom-yellow' : 'bg-transparent'
       } hover:bg-green-300 focus:outline-none focus:ring-2 focus:ring-yellow-500 transition duration-300 ease-in-out`}
     >
@@ -49,7 +49,7 @@ const NavBar: React.FC<NavBarProps> = ({page, park}) => {
 
   return (
     <div className="fixed top-0 w-full z-50 px-4 mt-2">
-      <div className="bg-custom-NavBarGreen p-1 rounded-b-lg rounded-t-lg shadow-md">
+      <div className="bg-custom-NavBarGreen p-2 rounded-b-lg rounded-t-lg shadow-md">
         <div className="flex justify-between items-center">
           <NavButton label="← Back to Map" onClick={handleMap} />
           <div className="flex gap-2">
