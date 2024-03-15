42e56a0ab54beb6f79748f7f48cd34ee789ea887
lillian-tran <lilliantran17@gmail.com>
2024-03-15 12:12:29 -0400

    work in progress menu button and screen size

--- src/app/arnold-arboretum/attractions/page.tsx
+++ src/app/arnold-arboretum/attractions/page.tsx
@@ -2,7 +2,6 @@
     arnold-arboretum/attractions
 */
 'use client'
-
 import React from "react";
 import ParkTitle from "@/components/ParkTitle";
 import MenuButton from "@/components/MenuButton";
@@ -10,16 +9,21 @@ import ParkNav from "@/components/ParkNav";
 import arnoldmap from '/public/img/arnoldmap.png';
 import menu_icon from '/public/img/menu_icon.png';
 import Image from 'next/image'
-
+import MenuButton2 from "@/components/MenuButton2";
 export default function ArnArbAttractions() {
-	return (
-		<div className="bg-custom-HistoryBackgroundGreen h-full w-full">
-				<div className='flex justify-center items-center h-screen'>
-					<ParkNav page={"Attractions"} park={"arnold-arboretum"}/>
-					<Image className="absolute top-20 right-10" src={menu_icon} alt="Menu Icon" width={150} height={150}/>
-					<Image className='m-auto' src={arnoldmap} alt="Arnold Arboretum Map" width={800} height={800}/>
-					<p className="text-white font-[orelega] text-6xl absolute bottom-0 left-0 ml-10 mb-20">Arnold Arboretum</p>
-				</div>
-		</div>
-	)
+    const options = ['Option 1', 'Option 2', 'Option 3'];
+    return (
+        <div className="bg-custom-HistoryBackgroundGreen h-full w-full">
+                <div className='flex justify-center items-center h-screen'>
+                    <ParkNav page={"Attractions"} park={"arnold-arboretum"}/>
+                    <MenuButton2 options={options} />
+                    {/* <Image className="absolute top-20 right-10" src={menu_icon} alt="Menu Icon" width={150} height={150}/> */}
+                    <Image className='m-auto' src={arnoldmap} alt="Arnold Arboretum Map" width={800} height={800}/>
+                    <p className="text-white font-[orelega] text-6xl absolute bottom-0 left-0 ml-10 mb-20">Arnold Arboretum</p>
+                </div>
+                <div style={{ height: '200vh' }}>
+                    <p>Hello!</p>
+                </div>
+        </div>
+    )
 }
---
+++ src/components/MenuButton2.tsx
@@ -0,0 +1,45 @@
+'use client'
+import Image from 'next/image'
+import React, { useState } from 'react';
+import menu_icon from '/public/img/menu_icon.png'
+interface DropdownProps {
+    options: string[];
+}
+  
+const MenuButton2: React.FC<DropdownProps> = ({ options }) => {
+    const [isOpen, setIsOpen] = useState(false);
+  
+    const toggleDropdown = () => {
+      setIsOpen(!isOpen);
+    };
+  
+    return (
+      <div className="fixed top-20 right-0 mt-4 mr-4">
+        <div className="relative">
+        <button
+          className="rounded inline-flex items-center"
+          onClick={toggleDropdown}
+        >
+          {}
+          <Image src={menu_icon} alt="Menu Icon" width={150} height={150}/>
+          {}
+        </button>
+        </div>
+        {isOpen && (
+          <div className="absolute right-0 mt-2 w-48 bg-custom-NavBarGreen rounded-lg shadow-xl">
+            {options.map((option, index) => (
+              <a
+                key={index}
+                href="#"
+                className="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white border-b-2 border-black"
+              >
+                {<p>option</p>}
+              </a>
+            ))}
+          </div>
+        )}
+      </div>
+    );
+  };
+  
+export default MenuButton2;
