4a6b6df35632616348442b27cf67f84a96f4a598
lillian-tran <lilliantran17@gmail.com>
2024-03-16 12:43:22 -0400

    added image for map

--- src/app/arnold-arboretum/attractions/page.tsx
+++ src/app/arnold-arboretum/attractions/page.tsx
@@ -1,6 +1,3 @@
-/*
-    arnold-arboretum/attractions
-*/
 'use client'
 import React from "react";
 import ParkTitle from "@/components/ParkTitle";
@@ -11,27 +8,35 @@ import menu_icon from '/public/img/menu_icon.png';
 import Image from 'next/image'
 import MenuButton2 from "@/components/MenuButton2";
 import SingleAttraction from "@/components/SingleAttraction";
-
-
 export default function ArnArbAttractions() {
     const options = ['Option 1', 'Option 2', 'Option 3'];
     return (
-	
+    
         <div className="bg-custom-HistoryBackgroundGreen h-full w-full">
-                <div className='flex justify-center items-center h-screen'>
-                    <ParkNav page={"Attractions"} park={"arnold-arboretum"}/>
-		    <div className="relative top-45 left-10">
-		    	<SingleAttraction number={10} image={'arnoldmap.png'} />
-		    </div>
-                    <MenuButton2 options={options} />
-                    {/* <Image className="absolute top-20 right-10" src={menu_icon} alt="Menu Icon" width={150} height={150}/> */}
+            <div className='flex justify-center items-center h-screen'>
+                <ParkNav page={"Attractions"} park={"arnold-arboretum"}/>
+                <MenuButton2 options={options} />
+                {/* <Image className="absolute top-20 right-10" src={menu_icon} alt="Menu Icon" width={150} height={150}/> */}
+                <div>
                     <Image className='m-auto' src={arnoldmap} alt="Arnold Arboretum Map" width={800} height={800}/>
-		    {/* Font Orelega is broken */}
-                    <p className="text-white font-orelega text-6xl absolute bottom-0 left-0 ml-10 mb-20">Arnold Arboretum</p>
+                    <div className="relative bottom-60 left-20">
+                        <SingleAttraction number={10} image={'/img/gray_box.jpeg'} />
+                    </div>
+                    <div className="relative bottom-60 left-80">
+                        <SingleAttraction number={11} image={'/img/gray_box.jpeg'} />
+                    </div>
+                    <div className="relative bottom-96 left-96">
+                        <SingleAttraction number={12} image={'/img/gray_box.jpeg'} />
+                        <div className="relative left-40 bottom-20">
+                            <SingleAttraction number={13} image={'/img/gray_box.jpeg'} />
+                        </div>
+                    </div>
                 </div>
-                <div className='h-200vh bg-custom-HistoryBackgroundGreen h-full w-full'>
-                    <p>Hello!</p>
-                </div>
+                {/* Font Orelega is broken */}
+                <p className="text-white font-orelega text-6xl absolute bottom-0 left-0 ml-10 mb-20">Arnold Arboretum</p>
+            </div>
+            <div className='h-200vh bg-custom-HistoryBackgroundGreen h-full w-full'> {/* Lengthen page */}
+            </div>
         </div>
     )
 }
