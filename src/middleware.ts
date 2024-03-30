d49b79b607b1ddfa450f5f3bb435a82aa6501356
Vedant Modi <vedantmodi@gmail.com>
2024-03-30 10:45:10 -0400

    Completed authentication
    Co-authored-by: alexli24 <alexli24@users.noreply.github.com>

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,29 +1,28 @@
 import { NextRequest, NextResponse } from "next/server";
-
+/* Update paths that need authentication here */
 export const config = {
-  matcher: ["/", "/admin"],
+  matcher: ["/admin"],
 };
 
 export function middleware(req: NextRequest) {
   const basicAuth = req.headers.get("authorization");
-  const url = req.nextUrl;
 
   if (basicAuth) {
     const authValue = basicAuth.split(" ")[1];
     const [user, pwd] = atob(authValue).split(":");
-    console.log("user ", user);
-    console.log("pwd ", pwd);
 
-    const validUser = process.env.BASIC_AUTH_USER;
+    const validUser = process.env.BASIC_AUTH_USER; 
     const validPassWord = process.env.BASIC_AUTH_PASSWORD;
 
     if (user === validUser && pwd === validPassWord) {
-      console.log("Logged in!")
-      return NextResponse.next();
+      return NextResponse.next(); /* This passes you to the protected routes */
     }
   }
 
-  url.pathname = "/";
-
-  return NextResponse.rewrite(url);
+  return new Response("Authentication Required!", {
+    status: 401,
+    headers: {
+      "WWW-Authenticate": "Basic realm='admin'",
+    },
+  });
 }
