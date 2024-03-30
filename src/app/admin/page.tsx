86b283cba4bf1d420d47924b02aae71f631568ff
Vedant Modi <vedantmodi@gmail.com>
2024-03-30 10:45:10 -0400

    Completed authentication
    Co-authored-by: alexli24 <alexli24@users.noreply.github.com>

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,13 @@
-import { RedirectType } from 'next/navigation'
+'use client'
 
-export default function Page() {
-	return <></>
+import React from "react"
+
+export default function AdminPage() {
+    return (
+        <div>
+            {
+                <h1>Welcome Admin!</h1>
+            }
+        </div>
+    )
 }
