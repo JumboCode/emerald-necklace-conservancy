ce0c66a315b0afdbd963cb989c19be2c8aab122e
AlexW-F <awilliamsferreira@gmail.com>
2024-04-21 20:27:46 -0400

    Fixed the Modal overlaping on page.tsx (DAN LETS GOOOO)

--- src/components/DirectionsContainer.tsx
+++ src/components/DirectionsContainer.tsx
@@ -1,67 +1,54 @@
-
-'use client'
-
+'use client';
 import React from "react";
-import ParkNav from "./ParkNav" 
-import Image from 'next/image'
-import background from '/public/img/white_shapes.png'
-
+import ParkNav from "./ParkNav";
+import Image from 'next/image';
+import background from '/public/directions_bg.png';
 interface DirectionsContainerProps {
     parkURL: string;
     parkName: string;
 }
-
-
 const DirectionsContainer: React.FC<DirectionsContainerProps> = ({ parkURL, parkName }) => {
-    
     function titleCaseWithSpaces(input: string): string {
         return input
-            .split('-') 
+            .split('-')
             .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()) // Capitalize the first letter of each word
             .join(' '); // Rejoin the words with spaces
     }
-
-
-	return (
-        <div>                    
+    return (
+        <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden">
             <ParkNav page={"Directions"} park={parkName} />
-            <div className="flex items-center px-16 pt-64 justify-center min-h-screen h-screen w-screen bg-custom-HistoryBackgroundGreen z-0 ">
-                <div className="absolute inset-0">
-                    <Image src={background} alt="Splash Background" className="absolute w-full h-full object-cover z-1" />
+            <div className="flex flex-grow items-stretch justify-between w-full bg-custom-HistoryBackgroundGreen">
+                <div className="absolute inset-0 z-0">
+                    <Image className="w-screen h-screen" src={background} alt="Splash Background" layout="fill" objectFit="cover" />
                 </div>
-                    <div className="relative w-1/2 h-screen float-left">
-                        <div className="map-container">
-                            <iframe
-                                width="600"
-                                height="450"
-                                loading="lazy"
-                                src={parkURL}>
-                            </iframe>
-                        </div>
+                <div className="relative w-1/2 h-full flex justify-center items-center">
+                    <iframe
+                        className="w-full h-3/5 px-12"
+                        src={parkURL}
+                        frameBorder="0"
+                        loading="lazy">
+                    </iframe>
+                </div>
+                <div className="relative w-1/2 h-full flex flex-col space-y-8 text-center p-16 overflow-auto pt-32 text-[#567534] text-lg">
+                    <div>
+                        <h1 className="text-3xl pb-4">{titleCaseWithSpaces(parkName)}</h1>
+                        <p>Franklin Park is located by the Charles River. Lorem ipsum.</p>
                     </div>
-                    <div className="relative w-1/2 h-screen float-right p-4 space-y-8">
-                        <div>
-                            <h1 className="text-xl">{titleCaseWithSpaces(parkName)}</h1>
-                            <p>Franklin Park is located by the Charles River. Lorem ipsum.</p>
-                        </div>
-                        <div>
-                            <h2 className="text-lg">By Walking</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-                        </div>
-                        <div> 
-                            <h3 className="text-lg">By Driving</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-                        </div>
-                        <div>
-                            <h4 className="text-lg">By Transit</h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-                        </div>
+                    <div>
+                        <h2 className="text-xl">By Walking</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                    <div>
+                        <h3 className="text-xl">By Driving</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                    <div>
+                        <h4 className="text-xl">By Transit</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                     </div>
                 </div>
-                </div>
-           
-
-	)
+            </div>
+        </div>
+    );
 }
-
 export default DirectionsContainer;
