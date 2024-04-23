2be0898e2f866ba1abe455425d21946b75c7b2c1
Snw125 <snowysnw123@gmail.com>
2024-04-23 11:08:39 -0400

    nice looking scroll on attractions page

--- src/app/jamaica-pond/attractions/page.tsx
+++ src/app/jamaica-pond/attractions/page.tsx
@@ -17,6 +17,31 @@ import { useRef } from 'react'
 import BacktoTopButton from '@/components/BacktoTopButton'
 
 export default function JamaicaAttractions () {
+    // const preScroll = useRef(null);
+    // const elemRef = useRef(null);
+    // const [scale, setScale] = useState(1.05);
+ 
+    // useLayoutEffect(() => {
+    //     const botPos = (element) => element.getBoundingClientRect().bottom;
+    //     const onScroll = () => {
+    //       const divBotPos = botPos(elemRef.current);
+    //       const scrollPos = preScroll.current > window.scrollY;
+    //       preScroll.current = window.scrollY;
+    //       if (scrollPos && divBotPos > window.innerHeight) {
+    //         setScale(1);
+    //         return;
+    //       }
+    //       if (divBotPos < window.innerHeight) {
+    //         setScale(1.1);
+    //       }
+    //     };
+    
+    //     window.addEventListener("scroll", onScroll);
+    //     return () => window.removeEventListener("scroll", onScroll);
+    //   }, []);
+  
+  
+  
   /* Update for location */
   const options = [
     'Jamaica Pond Boathouse/Bandstand',
@@ -40,8 +65,12 @@ export default function JamaicaAttractions () {
         <ParkNav page={'Attractions'} park={'jamaica-pond'} />
         <MenuButton2 options={options} />
         {/* <Image className="absolute top-20 right-10" src={menu_icon} alt="Menu Icon" width={150} height={150}/> */}
-        <div>
-          <Image
+        <div className="parallax__fixed fixed">
+            {/*  style={{
+            transition: "transform 1000ms ease-in-out",
+            transform: `scale(${scale*1.2}) translate(${scale}%, 5%)`
+            }} */}
+             <Image
             className='m-auto mt-16'
             src={bgmap}
             alt='Jamaica Pond Map' /* Update: New alt */
@@ -97,12 +126,16 @@ export default function JamaicaAttractions () {
           {/* Update: Name */}
 		  Jamaica Pond{' '}
         </p>
-      </div>
 
-      <div className='bg-custom-HistoryBackgroundGreen w-full'>
+
+      
+      
+      <div id="group1" class="parallax parallax__group" className='bg-custom-HistoryBackgroundGreen w-full'>
+        <div class="h-[50vh]"> 
+        </div>
         {' '}
 		{/* Update refs here */}
-        <div ref={descriptionRefs[0]} id='0'>
+        <div class="parallax__layer parallax__layer--base" ref={descriptionRefs[0]} id='0'>
           <AttractionInformation
             attractionNumber={ATTRACTION_START + 0}
             attractionName={'Jamica Pond Boathouse/Bandstand'}
