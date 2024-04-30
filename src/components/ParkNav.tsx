1180086d848d5a5af5b3f130a7fabf09e1c12c79
Benjamin-Skinner <benjamin.kean.skinner@gmail.com>
2024-04-30 12:45:52 -0400

    Switch to window.location.href

--- src/components/ParkNav.tsx
+++ src/components/ParkNav.tsx
@@ -1,84 +1,99 @@
 'use client'
 
-import React from 'react';
+import React from 'react'
 import { useRouter } from 'next/navigation'
 
 type MapButtonProps = {
-    selected?: boolean;
-    onClick: () => void;
-};
+	selected?: boolean
+	onClick: () => void
+}
 
 type NavButtonProps = {
-  label: string;
-  selected?: boolean;
-  onClick: () => void;
-};
+	label: string
+	selected?: boolean
+	onClick: () => void
+}
 
 const NavButton: React.FC<NavButtonProps> = ({ label, selected, onClick }) => {
-  return (
-    <button
-      onClick={onClick}
-      className={`px-6 py-4 text-sm text-xl p-0 ${
-        selected ? 'bg-custom-yellow' : 'bg-transparent'
-      } hover:bg-green-300 focus:outline-none focus:ring-2 focus:ring-yellow-500 transition duration-300 ease-in-out`}
-    >
-      {label}
-    </button>
-  );
+	return (
+		<button
+			onClick={onClick}
+			className={`px-6 py-4 text-sm text-xl p-0 ${
+				selected ? 'bg-custom-yellow' : 'bg-transparent'
+			} hover:bg-green-300 focus:outline-none focus:ring-2 focus:ring-yellow-500 transition duration-300 ease-in-out`}
+		>
+			{label}
+		</button>
+	)
 }
 
 type NavBarProps = {
-	park: string;
-	page: 'History' | 'Attractions' | 'Directions' | 'Email Signup';
-};
+	park: string
+	page: 'History' | 'Attractions' | 'Directions' | 'Email Signup'
+}
 
-const NavBar: React.FC<NavBarProps> = ({page, park}) => {
-
-    const router = useRouter()
+const NavBar: React.FC<NavBarProps> = ({ page, park }) => {
+	const router = useRouter()
 
 	const handleHistory = () => {
 		if (page !== 'History') {
-			router.push(`/${park}`);
+			router.push(`/${park}`)
 		}
 	}
 
 	const handleAttractions = () => {
 		if (page !== 'Attractions') {
-			router.push(`/${park}/attractions`);
+			window.location.href = `/${park}/attractions`
 		}
 	}
 
 	const handleDirections = () => {
 		if (page !== 'Directions') {
-			router.push(`/${park}/directions`);
+			window.location.href = `/${park}/directions`
 		}
 	}
 
-    const handleSignup = () => {
-        if (page !== 'Email Signup') {
-            router.push('/email')
-        }
-    }
-
-	const handleMap = () => {
-		router.push('/map')
+	const handleSignup = () => {
+		if (page !== 'Email Signup') {
+			window.location.href = `/email`
+		}
 	}
 
-  return (
-    <div className="fixed top-0 w-full z-50 px-4 mt-2">
-      <div className="bg-custom-NavBarGreen p-2 rounded-b-lg rounded-t-lg shadow-md">
-        <div className="flex justify-between items-center">
-          <NavButton label="← Back to Map" onClick={handleMap} />
-          <div className="flex gap-2">
-            <NavButton label="History" selected={page === 'History'} onClick={handleHistory} />
-            <NavButton label="Attractions" selected={page === 'Attractions'} onClick={handleAttractions} />
-            <NavButton label="Directions" selected={page === 'Directions'} onClick={handleDirections} />
-            <NavButton label="Signup" selected={page ==='Email Signup'} onClick={handleSignup} />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	const handleMap = () => {
+		window.location.href = '/'
+	}
+
+	return (
+		<div className="fixed top-0 w-full z-50 px-4 mt-2">
+			<div className="bg-custom-NavBarGreen p-2 rounded-b-lg rounded-t-lg shadow-md">
+				<div className="flex justify-between items-center">
+					<NavButton label="← Back to Map" onClick={handleMap} />
+					<div className="flex gap-2">
+						<NavButton
+							label="History"
+							selected={page === 'History'}
+							onClick={handleHistory}
+						/>
+						<NavButton
+							label="Attractions"
+							selected={page === 'Attractions'}
+							onClick={handleAttractions}
+						/>
+						<NavButton
+							label="Directions"
+							selected={page === 'Directions'}
+							onClick={handleDirections}
+						/>
+						<NavButton
+							label="Signup"
+							selected={page === 'Email Signup'}
+							onClick={handleSignup}
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default NavBar;
+export default NavBar
